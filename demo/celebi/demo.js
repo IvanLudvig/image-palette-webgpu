eee1c507ddb1b3421454e074898d516ae6cb12f8
Ivan Ludvig <ivanludvigdev@gmail.com>
2025-01-30 23:52:50 +0100

    celebi

---
+++ demo/celebi/demo.js
@@ -0,0 +1,15 @@
+import { extractDominantColors } from '../../src/celebi/index.js';
+import { renderColors, setupImageUploadListener } from '../demo_utils.js';
+
+const image = document.querySelector('img');
+const imageUpload = document.getElementById('image-upload');
+const palette = document.getElementById('color-palette');
+
+async function run() {
+    palette.innerHTML = '';
+    const colors = await extractDominantColors(image);
+    renderColors(palette, colors);
+}
+
+setupImageUploadListener(imageUpload, image, run);
+run();
---
+++ src/celebi/index.js
@@ -0,0 +1,48 @@
+import { extractDominantColorsGPU as extractWuColorsGPU } from '../wu/index.js';
+import { extractDominantColorsGPU as extractKMeansColorsGPU } from '../kmeans/index.js';
+import params from '../params.js';
+import { floatArrayToHex } from '../utils/color_utils.js';
+
+export async function extractDominantColorsGPU(device, source) {
+    const wuResultsBuffer = await extractWuColorsGPU(device, source);
+    const resultsBuffer = await extractKMeansColorsGPU(device, source, wuResultsBuffer);
+    return resultsBuffer;
+}
+
+export async function extractDominantColors(imageSource) {
+    const adapter = await navigator.gpu?.requestAdapter();
+    const device = await adapter?.requestDevice();
+    if (!device) {
+        throw new Error('WebGPU not supported');
+    }
+
+    const source = await createImageBitmap(imageSource, { colorSpaceConversion: 'none' });
+    const resultsBuffer = await extractDominantColorsGPU(device, source);
+    
+    const stagingResultsBuffer = device.createBuffer({
+        size: 3 * params.K * Float32Array.BYTES_PER_ELEMENT,
+        usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ
+    });
+    
+    const encoder = device.createCommandEncoder();
+    encoder.copyBufferToBuffer(
+        resultsBuffer, 0,
+        stagingResultsBuffer, 0,
+        3 * params.K * Float32Array.BYTES_PER_ELEMENT
+    );
+    device.queue.submit([encoder.finish()]);
+
+    await stagingResultsBuffer.mapAsync(GPUMapMode.READ, 0, 3 * params.K * Float32Array.BYTES_PER_ELEMENT);
+    const mappedData = stagingResultsBuffer.getMappedRange();
+    const colors = new Float32Array(mappedData.slice(0));
+    stagingResultsBuffer.unmap();
+
+    const validColors = [];
+    for (let i = 0; i < colors.length; i += 3) {
+        if (!isNaN(colors[i]) && !isNaN(colors[i + 1]) && !isNaN(colors[i + 2])) {
+            validColors.push(colors[i], colors[i + 1], colors[i + 2]);
+        }
+    }
+
+    return floatArrayToHex(new Float32Array(validColors));
+}
